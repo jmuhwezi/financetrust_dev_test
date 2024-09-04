cc34c68a4bb132b934369c9539b019b704fb9de1
androidcode33 <androidcode33@gmail.com>
2024-09-04 18:59:09 +0300

    add app end-points

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+from recipes.views import RecipesFileUploadView, FindUniqueRecipesView, CountUniqueRecipeOccurencesView, \
+FindMostDeliveredRecipeByPostCodeView, FindMatchingRecipesView
 
 # app end-points
 urlpatterns =[
-
+    path('upload-recipes-file/', RecipesFileUploadView.as_view(), name='upload-recipes-file'),
+    path('find-unique-recipes/', FindUniqueRecipesView.as_view(), name='find-unique-recipes'),
+    path('count-unique-recipes-occurrances/', CountUniqueRecipeOccurencesView.as_view(), name='count-unique-recipes-occurrances'),
+    path('find-most-delivered-recipe/', FindMostDeliveredRecipeByPostCodeView.as_view(), name='find-most-delivered-recipe'),
+    path('find-matching-recipes/', FindMatchingRecipesView.as_view(), name='find-matching-recipes')
 ]
